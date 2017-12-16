70508b78c82a383aa9cd98046cd270c90ace3596
Anshul Dubey <anshuldubey2166@gmail.com>
2017-12-16 16:30:15 +0530

    Designed carousel and exit button

--- scripts/app.js
+++ scripts/app.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
 	$('#show-carousel').click(function(){
 		$('.center-data').toggleClass("inactive");
 	});
+	$('.close').click(function(){
+		$('.center-data').toggleClass('inactive');
+	})
 })
