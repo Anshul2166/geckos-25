46876d422540117a6497b14413970f453ee29582
Christian Vibar <cafvibar13@gmail.com>
2017-12-17 16:56:41 +0800

    Added bacground image on load event

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
-$(document).ready(function(){
+$(window).on("load", function(){
 
 	  //Slide Center Block upward upon loading
       $("#center-block").show("slide", { direction: "down" }, 500);
+      $("body").css("background-image", "url('https://i.imgur.com/dcHO7hg.jpg')");
       
 });
