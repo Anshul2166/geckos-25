fb5b9f437e8e3cd316715cad4186d0dc5171e74c
Christian Vibar <cafvibar13@gmail.com>
2017-12-18 20:43:36 +0800

    Added grey background image on initial load

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,10 @@
 $(window).on("load", function(){
 
-	  //Slide Center Block upward upon loading
-      $("#center-block").show("slide", { direction: "down" }, 500);
-      $("body").css("background-image", "url('https://i.imgur.com/dcHO7hg.jpg')");
+	//Add background image once page loads
+	$("body").css("background-image", "url('https://i.imgur.com/dcHO7hg.jpg')");
+
+	//Slide Center Block upward upon loading
+    $("#center-block").show("slide", { direction: "down" }, 500);
+      
       
 });
