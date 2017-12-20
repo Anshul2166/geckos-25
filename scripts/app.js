185e69edb707073dee564c25b6008a47bc93f22d
Anshul Dubey <anshuldubey2166@gmail.com>
2017-12-20 11:17:01 +0530

    Modal Added

--- scripts/app.js
+++ scripts/app.js
@@ -1,21 +1,23 @@
-$(document).ready(function(){
-	$('#show-carousel').click(function(){
-		$('.center-data').toggleClass("inactive"); //inactive class makes the display:none
-	});
-	$('#close-carousel').click(function(){
-		$('.center-data').toggleClass('inactive');
-	});
-	$('#my-profile').click(function(){
-		$('.container-fluid').css('filter','blur(5px)');
-	});
+$(document).ready(function() {
+    $('#show-carousel').click(function() {
+        $('.center-data').toggleClass("inactive"); //inactive class makes the display:none
+    });
+    $('#close-carousel').click(function() {
+        $('.center-data').toggleClass('inactive');
+    });
+    $('#my-profile').click(function() {
+        // $('.container-fluid').css('filter','blur(5px)');
+    });
 });
-$(window).on("load", function(){
+$(window).on("load", function() {
 
-	//Add background image once page loads
-	$("body").css("background-image", "url('https://i.imgur.com/dcHO7hg.jpg')");
+    //Add background image once page loads
+    $("body").css("background-image", "url('https://i.imgur.com/dcHO7hg.jpg')");
 
-	//Slide Center Block upward upon loading
-	$("#center-block").show("slide", { direction: "down" }, 500);
+    //Slide Center Block upward upon loading
+    $("#center-block").show("slide", {
+        direction: "down"
+    }, 500);
 
 
 });
